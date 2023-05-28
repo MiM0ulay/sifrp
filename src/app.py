cfb5cd31656994c12c64e8e4550752c331cffa25
MiM0ulay <m.idmoulay@outlook.com>
2023-05-28 13:40:29 +0200

    minimal game

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
+import random
+
+
 class GameEngine:
     def __init__(self):
-        pass
+        self.player_name = ""
         # Initialize the game engine and its internal state
 
     def start_game(self):
-        pass
+        self.print_header()
+
+        self.create_character()
+
+        self.handle_input()
         # Start the game and handle the main game loop
 
     def create_character(self):
-        pass
+        print("Character Creation:")
+        player_name = input("Enter your character name: ")
+        self.player = Character(player_name)
+        self.player.generate_attributes()
+        self.player.display_info()
         # Prompt the player for character creation and customization
 
     def handle_scheming(self):
@@ -16,12 +27,14 @@ class GameEngine:
         # Manage the political scheming mechanics and interactions with noble houses
 
     def undertake_quest(self):
-        pass
         # Handle the quests and missions, including espionage, information gathering, and goal achievement
+         # Simulate a dice roll using the DiceEngine
+        dice_count = 2  # Example: Roll 2 dice
+        sides = 6  # Example: 6-sided dice
+        roll_result = DiceEngine.roll(dice_count, sides)
+        print(f"You rolled: {roll_result}")
 
-    def handle_consequences(self):
-        pass
-        # Process the consequences of player decisions and update the game state
+        # ...
 
     def interact_with_npcs(self):
         pass
@@ -32,23 +45,79 @@ class GameEngine:
         # Display the current game state, including character information, quests, and relationships
 
     def handle_input(self):
-        pass
+        while True:
+            command = input("Enter a command (quit to exit, quest to embark on a quest): ")
+
+            if command.lower() == "quit":
+                print("Exiting the game...")
+                break
+            elif command.lower() == "quest":
+                self.player.embark_on_quest()
+            else:
+                print("Invalid command. Please try again.")
         # Handle user input and trigger appropriate actions based on the current game state
 
     def handle_exit(self):
         pass
         # Clean up resources and gracefully exit the game
 
+    def print_header(self):
+        header = """
+               ####################################################
+               #                                                  #
+               #       Welcome to A Song of Ice and Fire RPG!       #
+               #                                                  #
+               ####################################################
+               """
+        print(header)
+
 
 class Character:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.strength = 0
+        self.agility = 0
+        self.intelligence = 0
         # Initialize the character with the given name
 
-    def customize_attributes(self):
+    def embark_on_quest(self):
+        print(f"{self.name} embarks on a quest!")
+
+        quest_difficulty = random.randint(1, 10)
+        print(f"The quest difficulty is {quest_difficulty}.")
+
+        success_threshold = self.strength + self.agility + self.intelligence
+        if success_threshold >= quest_difficulty:
+            print("Quest completed successfully!")
+        else:
+            print("Quest failed!")
+
+        print("Quest ends.")
+
+    def handle_consequences(self):
         pass
+        # Process the consequences of player decisions and update the game state
+
+    def customize_attributes(self):
+        print("Customize your attributes:")
+        self.strength = self.roll_attribute()
+        self.agility = self.roll_attribute()
+        self.intelligence = self.roll_attribute()
+
+        print("Attribute customization complete!")
+        self.display_info()
         # Prompt the player to customize the character's attributes
 
+    def generate_attributes(self):
+        self.strength = random.randint(1, 6)
+        self.agility = random.randint(1, 6)
+        self.intelligence = random.randint(1, 6)
+
+
+    def roll_attribute(self):
+        rolls = [random.randint(1, 6) for _ in range(3)]
+        return sum(sorted(rolls)[1:])
+    
     def manage_relationships(self):
         pass
         # Manage the character's relationships with noble houses and other characters
@@ -66,7 +135,11 @@ class Character:
         # Process an event that affects the character's state
 
     def display_info(self):
-        pass
+        print("Character Information:")
+        print(f"Name: {self.name}")
+        print(f"Strength: {self.strength}")
+        print(f"Agility: {self.agility}")
+        print(f"Intelligence: {self.intelligence}")
         # Display the character's information, including attributes, relationships, and current quest
 
 
@@ -90,3 +163,24 @@ class Quest:
     def display_info(self):
         pass
         # Display information about the quest, including title, description, and objectives
+
+
+
+class DiceEngine:
+    @staticmethod
+    def roll(dice_count, sides):
+        rolls = [random.randint(1, sides) for _ in range(dice_count)]
+        return sum(rolls)
+
+
+def main():
+    # Create an instance of the GameEngine class
+    game = GameEngine()
+
+    # Start the game
+    game.start_game()
+
+
+if __name__ == "__main__":
+    # Call the main function when the script is executed
+    main()
