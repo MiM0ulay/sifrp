c3dacc85744984bb5f6d0d9cf96856d4a97599d2
MiM0ulay <m.idmoulay@outlook.com>
2023-05-28 12:55:25 +0200

    add pass to empyt method

--- src/app.py
+++ src/app.py
@@ -1,70 +1,92 @@
 class GameEngine:
     def __init__(self):
+        pass
         # Initialize the game engine and its internal state
 
     def start_game(self):
+        pass
         # Start the game and handle the main game loop
 
     def create_character(self):
+        pass
         # Prompt the player for character creation and customization
 
     def handle_scheming(self):
+        pass
         # Manage the political scheming mechanics and interactions with noble houses
 
     def undertake_quest(self):
+        pass
         # Handle the quests and missions, including espionage, information gathering, and goal achievement
 
     def handle_consequences(self):
+        pass
         # Process the consequences of player decisions and update the game state
 
     def interact_with_npcs(self):
+        pass
         # Manage interactions with dynamic NPCs, including their personalities and reactions
 
     def display_game_state(self):
+        pass
         # Display the current game state, including character information, quests, and relationships
 
     def handle_input(self):
+        pass
         # Handle user input and trigger appropriate actions based on the current game state
 
     def handle_exit(self):
+        pass
         # Clean up resources and gracefully exit the game
 
 
 class Character:
     def __init__(self, name):
+        pass
         # Initialize the character with the given name
 
     def customize_attributes(self):
+        pass
         # Prompt the player to customize the character's attributes
 
     def manage_relationships(self):
+        pass
         # Manage the character's relationships with noble houses and other characters
 
     def receive_quest(self, quest):
+        pass
         # Assign a new quest to the character
 
     def complete_quest(self, quest):
+        pass
         # Handle the completion of a quest and update character progression
 
     def handle_event(self, event):
+        pass
         # Process an event that affects the character's state
 
     def display_info(self):
+        pass
         # Display the character's information, including attributes, relationships, and current quest
 
 
 class Quest:
     def __init__(self, title, description, objectives, rewards):
+        pass
         # Initialize the quest with the given details
 
     def update_progress(self):
+        pass
         # Update the progress of the quest based on player actions
 
     def check_completion(self):
+        pass
         # Check if the quest's objectives have been completed
 
     def give_rewards(self):
+        pass
         # Provide rewards to the player upon quest completion
 
     def display_info(self):
+        pass
         # Display information about the quest, including title, description, and objectives
