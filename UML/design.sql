9032b0ae9df0f1756c3674c20a284409ff49fd1e
MiM0ulay <m.idmoulay@outlook.com>
2023-05-28 12:55:10 +0200

    add design

---
+++ UML/design.sql
@@ -0,0 +1,24 @@
++-------------------+              +------------------+
+|     GameEngine    |              |      Character   |
++-------------------+              +------------------+
+|                   |              |   - name         |
+|                   |              |   - attributes   |
+|                   |              |   - relationships|
+|                   |              |   - quests       |
+|                   |              +------------------+
+|   - characters    |<----------------------+
+|   - nobleHouses   |                       |
+|   - currentQuest  |                       |
+|   - gameEvents    |                       |
+|   - gameState     |                       |
++-------------------+                       |
+                                            |
+                                            |
+                                      +------------------+
+                                      |        Quest     |
+                                      +------------------+
+                                      |   - title        |
+                                      |   - description  |
+                                      |   - objectives   |
+                                      |   - rewards      |
+                                      +------------------+
